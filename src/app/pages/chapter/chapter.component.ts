0313d0394780e20250e382ed9a83776ce0ea793f
Ranyere <Ranyere@DESKTOP-5EIPQ1S>
2025-06-22 10:55:55 -0300

    Título da tela de capítulos

--- src/app/pages/chapter/chapter.component.ts
+++ src/app/pages/chapter/chapter.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import {velhoTestamento, novoTestamento} from '../../consts/books'
 import { ActivatedRoute } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-chapter',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './chapter.component.html',
   styleUrl: './chapter.component.scss'
 })
@@ -14,11 +15,32 @@ export class ChapterComponent {
   novoTestamento = novoTestamento;
   isNewTestament:boolean = false;
   book:string | null = "";
+  quantity: number | undefined = 0;
+  bookName: string | undefined = "";
+  chapters: number[] = []; 
 
   constructor(private route: ActivatedRoute){
    this.route.queryParamMap.subscribe(params => {
       this.isNewTestament = params.get('isNewTestament') == "true";
       this.book = params.get('book')
+      this.getData();
     });
+  }
+
+  getData(){
+    if(this.isNewTestament){
+      this.quantity = this.novoTestamento.find(x => x.nome == this.book)?.capitulos;
+      this.bookName = this.novoTestamento.find(x => x.nome == this.book)?.titulo;
+    }
+    else{
+      this.quantity = this.velhoTestamento.find(x => x.nome == this.book)?.capitulos;
+      this.bookName = this.velhoTestamento.find(x => x.nome == this.book)?.titulo;
+    }
+    
+    this.chapters = this.generateList(Number(this.quantity));
+  }
+
+  generateList(quantity:number) {
+    return Array.from({ length: quantity }, (_, i) => i + 1);
   }
 }
